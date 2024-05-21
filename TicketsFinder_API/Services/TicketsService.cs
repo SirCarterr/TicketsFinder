864d0ec0404d7e4bb343586dfa05b1f4e1c1e745
Hyzha Vladislav <vladalexg@ukr.net>
2024-05-21 16:26:58 +0300

    ChromeOptions added

--- TicketsFinder_API/Services/TicketsService.cs
+++ TicketsFinder_API/Services/TicketsService.cs
@@ -3,7 +3,6 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.Extensions;
 using OpenQA.Selenium.Support.UI;
 using System.Globalization;
-using System.Text.RegularExpressions;
 using TicketFinder_Models;
 using TicketsFinder_API.Services.IServices;
 
@@ -12,23 +11,49 @@ namespace TicketsFinder_API.Services
     public class TicketsService : ITicketsService
     {
         private readonly string url = "https://booking.uz.gov.ua/";
+        private readonly ILogger _logger;
+
+        private readonly string[] ua = 
+        {
+            "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"
+        };
+        
         private IWebDriver? driver;
+
+        public TicketsService(ILogger<TicketsService> logger)
+        {
+            _logger = logger;
+        }
 
         public List<TicketDTO> SearchTickets(string from, string to, string? date, string? time)
         {
             try
             {
+                ChromeOptions options = new();
+                options.AddArgument($"--user-agent={ua[new Random().Next(5)]}");
+                options.AddArgument("--window-size=1920,1080");
+                options.AddArgument("--start-maximized");
+                options.ImplicitWaitTimeout = TimeSpan.FromSeconds(10);
+                options.PageLoadStrategy = PageLoadStrategy.Normal;
+
                 //open site
-                driver = new ChromeDriver();
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                driver = new ChromeDriver(options);
                 driver.Navigate().GoToUrl(url);
 
                 //set "from" input value
                 driver.FindElement(By.Name("from-title")).SendKeys(from);
+                Thread.Sleep(250);
                 driver.FindElement(By.Id("from-autocomplete")).FindElement(By.TagName("li")).Click();
+                Thread.Sleep(250);
 
                 //set "to" input value
+                Thread.Sleep(250);
                 driver.FindElement(By.Name("to-title")).SendKeys(to);
+                Thread.Sleep(250);
                 driver.FindElement(By.Id("to-autocomplete")).FindElement(By.TagName("li")).Click();
 
                 //set date value
@@ -40,6 +65,7 @@ namespace TicketsFinder_API.Services
                     {
                         string dateParsedString = DateOnly.FromDateTime(dateParsed).ToString("yyyy-MM-dd");
                         var datePicker = driver.FindElement(By.XPath("//form//input[@name='date']"));
+                        Thread.Sleep(250);
                         driver.ExecuteJavaScript($"arguments[0].value = '{dateParsedString}'", datePicker);
                     }
                 }
@@ -47,6 +73,7 @@ namespace TicketsFinder_API.Services
                 //set time value
                 if (time != null)
                 {
+                    Thread.Sleep(250);
                     var timeElement = driver.FindElement(By.Name("time"));
                     var selectTime = new SelectElement(timeElement);
                     selectTime.SelectByValue(time);
@@ -56,6 +83,7 @@ namespace TicketsFinder_API.Services
                 driver.FindElement(By.XPath(@"//form//div[@class='button']//button")).Click();
 
                 //get list of train tickets
+                Thread.Sleep(250);
                 var list = driver.FindElements(By.XPath("//table[@class='train-table']//tr"));
 
                 List<TicketDTO> ticketDTOs = new();
@@ -108,6 +136,7 @@ namespace TicketsFinder_API.Services
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex.Message);
                 return new List<TicketDTO>();
             }
             finally
